1407fa19d07cc30896d14c1980d72b02f9cbd522
Nelly Kuznetsova <nelly@fwd.be>
2021-12-05 10:34:48 +0100

    Day 5 (optimized)

--- aoc2021/day05/main.go
+++ aoc2021/day05/main.go
@@ -5,66 +5,40 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 )
+
+const inputFormat = "%d,%d -> %d,%d"
 
 type coord struct {
 	x, y int
 }
 
-func (c *coord) moveHorizontally(other *coord) []coord {
-	x1, x2 := c.x, other.x
-	coords := make([]coord, 0)
-	if x1 < x2 {
-		for ; x1 <= x2; x1++ {
-			coords = append(coords, coord{x: x1, y: c.y})
+func (c *coord) move(other *coord) (path []coord) {
+	x1, x2, y1, y2 := c.x, other.x, c.y, other.y
+	for x1 != x2 || y1 != y2 {
+		path = append(path, coord{
+			x: x1,
+			y: y1,
+		})
+		if x1 > x2 {
+			x1--
+		} else if x1 < x2 {
+			x1++
 		}
-	} else {
-		for ; x1 >= x2; x1-- {
-			coords = append(coords, coord{x: x1, y: c.y})
-		}
-	}
-	return coords
-}
 
-func (c *coord) moveVertically(other *coord) []coord {
-	y1, y2 := c.y, other.y
-	coords := make([]coord, 0)
-
-	if y1 < y2 {
-		for ; y1 <= y2; y1++ {
-			coords = append(coords, coord{x: c.x, y: y1})
-		}
-	} else {
-		for ; y1 >= y2; y1-- {
-			coords = append(coords, coord{x: c.x, y: y1})
+		if y1 > y2 {
+			y1--
+		} else if y1 < y2 {
+			y1++
 		}
 	}
-	return coords
-}
-
-func (c *coord) String() string {
-	return fmt.Sprintf("x: %d, y: %d", c.x, c.y)
-}
-
-func (c *coord) parse(s string) {
-	strs := strings.Split(s, ",")
-	x, err := strconv.Atoi(strs[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	c.x = x
-	y, err := strconv.Atoi(strs[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	c.y = y
+	path = append(path, *other)
+	return path
 }
 
 type move struct {
 	from *coord
-	to *coord
+	to   *coord
 }
 
 func (m *move) isHorizontal() bool {
@@ -75,39 +49,7 @@ func (m *move) isVertical() bool {
 	return m.from.x == m.to.x
 }
 
-func (m *move) String() string {
-	return fmt.Sprintf("from: %d, to: %d", m.from, m.to)
-}
-
-func (m *move) parse(s string) {
-	strs := strings.Split(s, " -> ")
-	from, to := new(coord), new(coord)
-	from.parse(strs[0])
-	to.parse(strs[1])
-	m.from = from
-	m.to = to
-}
-
-func solution(moves []*move) int {
-	points := map[coord]int{}
-	for _, m := range moves {
-		coords := make([]coord, 0)
-		switch {
-		case m.isHorizontal():
-			coords = m.from.moveHorizontally(m.to)
-		case m.isVertical():
-			coords = m.from.moveVertically(m.to)
-		default: // for solution 2:
-			coordsX := m.from.moveHorizontally(m.to)
-			coordsY := m.from.moveVertically(m.to)
-			for i := range coordsX {
-				coords = append(coords, coord{x: coordsX[i].x, y: coordsY[i].y})
-			}
-		}
-		for _, c := range coords {
-			points[c] += 1
-		}
-	}
+func countPoints(points map[coord]int) int {
 	count := 0
 	for _, p := range points {
 		if p >= 2 {
@@ -115,6 +57,30 @@ func solution(moves []*move) int {
 		}
 	}
 	return count
+}
+
+func solution2(moves []*move) int {
+	points := map[coord]int{}
+	for _, m := range moves {
+		coords := m.from.move(m.to)
+		for _, c := range coords {
+			points[c] += 1
+		}
+	}
+	return countPoints(points)
+}
+
+func solution1(moves []*move) int {
+	points := map[coord]int{}
+	for _, m := range moves {
+		if m.isHorizontal() || m.isVertical() {
+			coords := m.from.move(m.to)
+			for _, c := range coords {
+				points[c] += 1
+			}
+		}
+	}
+	return countPoints(points)
 }
 
 func main() {
@@ -129,13 +95,18 @@ func main() {
 	moves := make([]*move, 0)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		m := new(move)
-		m.parse(scanner.Text())
-		moves = append(moves, m)
+		x1, y1, x2, y2 := 0, 0, 0, 0
+		if _, err = fmt.Sscanf(scanner.Text(), inputFormat, &x1, &y1, &x2, &y2); err != nil {
+			log.Fatal(err)
+		}
+		moves = append(moves, &move{
+			from: &coord{x: x1, y: y1},
+			to:   &coord{x: x2, y: y2},
+		})
 	}
-	//count := solution(moves)
-	//fmt.Printf("solution 1: ans: %d\n", count)
+	count := solution1(moves)
+	fmt.Printf("solution 1: ans: %d\n", count)
 
-	count := solution(moves)
+	count = solution2(moves)
 	fmt.Printf("solution 2: ans: %d\n", count)
 }
